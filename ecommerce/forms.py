02be0a567c64b8930cbe163ff83d3b0fc19376ed
Marcos Augusto <marcosdeantonio2020@gmail.com>
2025-11-24 22:25:45 -0300

    criando retorno de erro na UI do cadastro

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, DataRequired, EqualTo, Email
 
 
 class CadastroForm(FlaskForm):
-    usuario = StringField(label='Username')
-    email = StringField(label='Email')
-    senha1 = PasswordField(label='Senha')
-    senha2 = PasswordField(label='Confirmar senha')
+    usuario = StringField(label='Username', validators=[DataRequired(), Length(min=3, max=20)])
+    email = StringField(label='Email', validators=[Email(), DataRequired()])
+    senha1 = PasswordField(label='Senha', validators=[DataRequired(), Length(min=6, max=20)])
+    senha2 = PasswordField(label='Confirmar senha', validators=[DataRequired(), EqualTo('senha1')])
     submit = SubmitField(label='Cadastrar')
-
--- ecommerce/routes.py
+++ ecommerce/routes.py
@@ -1,6 +1,6 @@
-from ecommerce import app
-from flask import render_template
-from ecommerce.models import Item
+from ecommerce import app, db
+from flask import render_template, url_for, redirect, flash
+from ecommerce.models import Item, User
 from ecommerce.forms import CadastroForm
 
 
@@ -15,7 +15,23 @@ def product_page():
     return render_template('product_page.html', items=items, active_page='produtos')
 
 
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['GET', 'POST'])
 def page_cadastro():
     form = CadastroForm()
+    if form.validate_on_submit():
+        usuario = User(
+            usuario=form.usuario.data,
+            email=form.email.data,
+            senha=form.senha1.data
+        )
+
+        db.session.add(usuario)
+        db.session.commit()
+        return redirect(url_for('page_cadastro'))
+
+    if form.errors != {}:
+        for err_list in form.errors.values():
+            for err in err_list:
+                flash(f'Erro ao cadastrar usuário: {err}')
+
     return render_template('cadastro.html', form=form)
